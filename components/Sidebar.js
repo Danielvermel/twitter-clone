aa010c8535bfef6d5b259d180b0144eb9e86cb09
Daniel Leite <daniel.leite@comcarde.com>
2022-08-08 10:26:54 +0100

    add sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,14 +5,14 @@ import {BellIcon, BookmarkIcon, ClipboardIcon, DotsCircleHorizontalIcon, DotsHor
 
 export default function Sidebar() {
   return (
-    <div>
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
       {/* Twitter Logo */}
-      <div className="hoverEffect">
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
         <Image width="50" height="50" src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"></Image>
       </div>
       {/* Menu */}
-      <div className="">
-        <SidebarMenuItem text="Home" Icon={HomeIcon}/>
+      <div className=" mt-4 mb-2.5 xl:items-start">
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active />
         <SidebarMenuItem text="Explore" Icon={HashtagIcon}/>
         <SidebarMenuItem text="Notifications" Icon={BellIcon}/>
         <SidebarMenuItem text="Messages" Icon={InboxIcon}/>
@@ -23,19 +23,20 @@ export default function Sidebar() {
       </div>
       
       {/* Button */}
-      <button>Tweet</button>
+      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
       
       {/* Mini-Profile */}
       
-      <div className="hoverEffect">
+      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
         <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGfuZm7dCiVuQ/profile-displayphoto-shrink_800_800/0/1650874180152?e=1665014400&v=beta&t=kPifY9vq-ZtQiN_LZeHdNFHtbl24s_LsuEwPzDdefT0" 
-             alt="user image" className="h-20 rounded-full"
+             alt="user image" 
+             className="h-10 w-10 rounded-full xl:mr-2"
         />
-        <div className="">
-          <h4>Daniel Leite</h4>
-          <p>@danielvermel</p>
+        <div className="leading-5 hidden xl:inline">
+          <h4 className="font-bold">Daniel Leite</h4>
+          <p className="text-gray-500">@danielvermel</p>
         </div>
-        <DotsHorizontalIcon className="h-5"/>
+        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline"/>
       </div>
     </div>
   )
