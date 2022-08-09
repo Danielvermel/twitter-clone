52a62cb40777bbbb168d68b8ac8b9556293eb4f5
Daniel Leite <daniel.leite@comcarde.com>
2022-08-09 13:11:03 +0100

    add prettier

---
+++ components/Input.js
@@ -0,0 +1,11 @@
+export default function Input() {
+  return (
+    <div>
+      <img
+        src="https://media-exp1.licdn.com/dms/image/C4E03AQGfuZm7dCiVuQ/profile-displayphoto-shrink_800_800/0/1650874180152?e=1665014400&v=beta&t=kPifY9vq-ZtQiN_LZeHdNFHtbl24s_LsuEwPzDdefT0"
+        alt="user image"
+        className="h-10 w-10 rounded-full xl:mr-2"
+      />
+    </div>
+  )
+}
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,42 +1,58 @@
-import Image from "next/image";
-import SidebarMenuItem from "./SidebarMenuItem";
-import {HomeIcon} from "@heroicons/react/solid";
-import {BellIcon, BookmarkIcon, ClipboardIcon, DotsCircleHorizontalIcon, DotsHorizontalIcon, HashtagIcon, InboxIcon, UserIcon} from "@heroicons/react/outline";
+import Image from 'next/image'
+import SidebarMenuItem from './SidebarMenuItem'
+import { HomeIcon } from '@heroicons/react/solid'
+import {
+  BellIcon,
+  BookmarkIcon,
+  ClipboardIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+  HashtagIcon,
+  InboxIcon,
+  UserIcon,
+} from '@heroicons/react/outline'
 
 export default function Sidebar() {
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
       {/* Twitter Logo */}
       <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
-        <Image width="50" height="50" src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"></Image>
+        <Image
+          width="50"
+          height="50"
+          src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+        ></Image>
       </div>
       {/* Menu */}
       <div className=" mt-4 mb-2.5 xl:items-start">
         <SidebarMenuItem text="Home" Icon={HomeIcon} active />
-        <SidebarMenuItem text="Explore" Icon={HashtagIcon}/>
-        <SidebarMenuItem text="Notifications" Icon={BellIcon}/>
-        <SidebarMenuItem text="Messages" Icon={InboxIcon}/>
-        <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon}/>
-        <SidebarMenuItem text="Lists" Icon={ClipboardIcon}/>
-        <SidebarMenuItem text="Profile" Icon={UserIcon}/>
-        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon}/>
+        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+        <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+        <SidebarMenuItem text="Profile" Icon={UserIcon} />
+        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
       </div>
-      
+
       {/* Button */}
-      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
-      
+      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+        Tweet
+      </button>
+
       {/* Mini-Profile */}
-      
+
       <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGfuZm7dCiVuQ/profile-displayphoto-shrink_800_800/0/1650874180152?e=1665014400&v=beta&t=kPifY9vq-ZtQiN_LZeHdNFHtbl24s_LsuEwPzDdefT0" 
-             alt="user image" 
-             className="h-10 w-10 rounded-full xl:mr-2"
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C4E03AQGfuZm7dCiVuQ/profile-displayphoto-shrink_800_800/0/1650874180152?e=1665014400&v=beta&t=kPifY9vq-ZtQiN_LZeHdNFHtbl24s_LsuEwPzDdefT0"
+          alt="user image"
+          className="h-10 w-10 rounded-full xl:mr-2"
         />
         <div className="leading-5 hidden xl:inline">
           <h4 className="font-bold">Daniel Leite</h4>
           <p className="text-gray-500">@danielvermel</p>
         </div>
-        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline"/>
+        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
       </div>
     </div>
   )
